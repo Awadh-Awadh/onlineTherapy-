e8b67979feaa8730b31eb7bbe9001f44e119eaa0
Awadh-Awadh <awadhawadh84@yahoo.com>
2021-12-09 23:39:36 +0300

    deploy

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,6 @@ from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth import get_user_model
 from .models import CustomUser, Conditions, Profile
-from datetimepicker.widgets import DateTimePicker
 
 
 
